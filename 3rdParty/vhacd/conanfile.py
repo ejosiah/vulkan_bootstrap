0f79a07ad5e8bbbe2afab6e1c4bbefa029c019c7
Josiah Ebhomenye <joebhomenye@gmail.com>
2022-01-24 17:59:26 +0000

    added vhacd conan recipe

---
+++ 3rdParty/vhacd/conanfile.py
@@ -0,0 +1,52 @@
+from conans import ConanFile, CMake, tools
+
+
+class VhacdConan(ConanFile):
+    name = "vhacd"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Vhacd here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        self.run("git clone git@github.com:kmammou/v-hacd.git")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        tools.replace_in_file("v-hacd/src/CMakeLists.txt", "PROJECT(VHACD)",
+                              '''PROJECT(VHACD)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="v-hacd/src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("VHACD_Lib/inc/*.h", dst="include/vhacd", src="v-hacd/src", keep_path=False)
+        self.copy("VHACD_Lib/public/*.h", dst="include/vhacd", src="v-hacd/src", keep_path=False)
+        self.copy("*vhacd.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["vhacd"]
+
