1be95bfdd586218c2b8970b80fceb7a295cc5e9b
Josiah Ebhomenye <joebhomenye@gmail.com>
2022-10-17 22:56:25 +0100

    select between area and point light

--- VulkanBase/include/Mesh.h
+++ VulkanBase/include/Mesh.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d95799f10fc958e69ef502796cff245d40ee8cca84ff8774728ce4321181c07
-size 2842
+oid sha256:642f70657947e6dd44c68ac12585fff3cd241a093a2f33ef53d8e9ac7aab2488
+size 3478
--- examples/path_tracer/source/PathTracer.cpp
+++ examples/path_tracer/source/PathTracer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85df854682fa4152bb34bec95a7e913586e44ba596e240f6b8bd82d6635dee2e
-size 24745
+oid sha256:b5fc32cef834a81c4caae17dcf6e9402b6558d5ad385cc078ade10e9ee5fe2ab
+size 24764
