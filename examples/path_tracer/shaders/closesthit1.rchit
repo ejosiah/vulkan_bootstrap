e19d3fd695af2fb2770fd360696601ccf1bda689
Josiah Ebhomenye <joebhomenye@gmail.com>
2022-11-10 17:10:59 +0000

    checkpoint 0

---
+++ examples/path_tracer/shaders/closesthit1.rchit
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3491d5a2739ccaf42e0516e17827f578bf39370fbb6d2bcea340f203af147ddb
+size 1830
--- examples/path_tracer/source/PathTracer.cpp
+++ examples/path_tracer/source/PathTracer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:588d3815a780646eda3157673cd25e5316feed8f527dda870f72cdfd0210c937
-size 36127
+oid sha256:fc69d00922e0e41770d19e8c51ae8af6a3569e21e0e98f7403af5c15cf2c760c
+size 36129
