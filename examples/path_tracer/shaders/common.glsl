01b6ed2263e13f792d0a28e425017f607384d47a
Josiah Ebhomenye <joebhomenye@gmail.com>
2022-11-15 17:46:38 +0000

    checkpoint

--- examples/path_tracer/shaders/common.glsl
+++ examples/path_tracer/shaders/common.glsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a06b061b33a9bf78f6174520f776871b71d3fec6bf141da956d5baba72f00086
-size 4738
+oid sha256:510baca4c9902f76a28fd714e4b4679f1a5b1e7450cb536682176fe085e1c440
+size 4835
